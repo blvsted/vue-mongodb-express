0d8d42d69da6d99d8882132d7ad733a491bac6ec
Samuel <blvsted@Skys-Mac.local>
2023-09-23 02:01:10 +0200

    REST Server und MongoDB steht

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const postRoutes = require('./routes/post.routes');
-
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+// enable cors for all requests
+app.use(cors());
 app.use('/posts', postRoutes);
 
 app.listen(PORT, (error) => {
