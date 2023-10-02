b8d36275f234c6d118482fb018263b47ad329d96
Sky <145769153+blvsted@users.noreply.github.com>
2023-10-03 01:53:48 +0200

    Delete webapp directory

--- webapp/src/views/ShopView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is the shop page</h1>
-  </div>
-</template>
--- webapp/src/views/VideoView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="video">
-    <h1>This is the video page</h1>
-  </div>
-</template>
